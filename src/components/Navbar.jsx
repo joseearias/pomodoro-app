7c64a00bee4223b2a2aab3fc36f3b0735919e439
José Arias <j.ariascanto@gmail.com>
2023-03-16 23:11:55 -0600

    First commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+
+const Navbar = () => {
+  return (
+    <Box>
+      <AppBar>
+        <Toolbar>
+          <Typography>Hello</Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Navbar;
